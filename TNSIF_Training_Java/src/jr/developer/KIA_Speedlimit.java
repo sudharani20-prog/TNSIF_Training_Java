e17254db2975fa4f3c16ca9511061fea162496e2
sudharani20-prog <sudharanibukka2004@gmail.com>
2026-07-20 17:19:38 +0530

    first commit

---
+++ TNSIF_Training_Java/src/jr/developer/KIA_Speedlimit.java
@@ -0,0 +1,16 @@
+package jr.developer;
+
+public class KIA_Speedlimit {
+	 void disp_speedlimit()
+		{
+			System.out.println("All KIA have SpeedLimit");
+		}
+
+		 public static void main(String[] args) {
+			 KIA_Airbags bag=new KIA_Airbags();
+			 bag.disp_airbags();
+			 
+	}
+
+
+}
