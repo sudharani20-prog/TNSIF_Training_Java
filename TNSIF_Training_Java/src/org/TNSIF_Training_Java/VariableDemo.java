cb46b153a40ac5b0021c27e409e566b0cb20595f
sudharani20-prog <sudharanibukka2004@gmail.com>
2026-07-20 12:39:40 +0530

    first commit

---
+++ TNSIF_Training_Java/src/org/TNSIF_Training_Java/VariableDemo.java
@@ -0,0 +1,20 @@
+package org.TNSIF_Training_Java;
+
+public class VariableDemo {
+	
+	int price=10;
+	void print()
+	{
+		String msg="hello";
+		System.out.println(msg);
+	}
+	static String message="Hello Students";
+	
+	public static void main(String[] args) {
+		VariableDemo obj=new VariableDemo();
+		System.out.println(obj.price);
+		obj.print();
+		System.out.println(message);
+	}
+
+}
