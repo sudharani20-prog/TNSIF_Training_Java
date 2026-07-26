b1e6f6fc5fab9e24724705eccdb930280d4d07f2
Sudharani_S <sudharanibukka2004@gmail.com>
2026-07-26 14:48:40 +0530

    Add files via upload

---
+++ TNSIF_Training_Java/src/org/thsif/acc/c2tc/oops/EncapsulationDemo.java
@@ -0,0 +1,38 @@
+package org.thsif.acc.c2tc.oops;
+
+class Human{
+	
+	//variable
+		private String name;
+		private int age;
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+		public int getAge() {
+			return age;
+		}		
+	}
+	public class EncapsulationDemo {
+		public static void main(String[] args) {
+			Human obj=new Human();
+			obj.setName("Balaji");
+			obj.setAge(21);
+			System.out.println(obj.getName());
+			System.out.println(obj.getAge());
+			 
+			obj.setName("Hemanth");
+			obj.setAge(34);
+			System.out.println(obj.getName());
+			System.out.println(obj.getAge());
+			
+
+		}
+
+}
---
+++ TNSIF_Training_Java/src/org/tnsif/acc/c2tc/datatype_typecasting/ShoppingCart.java
@@ -0,0 +1,18 @@
+package org.tnsif.acc.c2tc.datatype_typecasting;
+
+public class ShoppingCart {
+	
+	public static void main(String[] args) {
+		//Implicit (smaller-larger)
+		
+		int quantity=3;
+		double pricePerItem=99.50d;
+		double total_price=quantity*pricePerItem;  //int->double automatically
+		
+		double discount=10.75;
+		
+
+	}
+
+
+}
