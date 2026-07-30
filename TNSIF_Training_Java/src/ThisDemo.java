9dd363e7e0bd1333d0d38d372e5b673b5b130bbd
Sudharani_S <sudharanibukka2004@gmail.com>
2026-07-30 15:19:56 +0530

    Add files via upload

---
+++ TNSIF_Training_Java/src/ThisDemo.java
@@ -0,0 +1,29 @@
+ package org.tnsif.acc.c2tc.methodoverriding_super_this_instanceof;
+
+
+class ProductBottle
+{
+	double price;
+	ProductBottle(double price)
+	{
+		this.price=price;
+	}
+	double calculateDiscount()
+	{ 
+		return this.price*0.10;
+		//400*0.10=40
+	}
+	double calculateTotalPrice()
+	{
+		return this.price-this.calculateDiscount();
+		//400-40=360
+	}
+}
+
+public class ThisDemo {
+	public static void main(String[] args) {
+		ProductBottle product=new ProductBottle(400);
+		System.out.println(" Final Price :"+product.calculateTotalPrice());
+	}
+
+}
