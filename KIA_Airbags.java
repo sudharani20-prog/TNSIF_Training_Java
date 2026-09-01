015d4bb800d3cafc181d9ddb1a3bd9d0331a2a26
Sudharani_S <sudharanibukka2004@gmail.com>
2026-09-01 12:55:31 +0530

    Add files via upload

---
+++ KIA_Airbags.java
@@ -0,0 +1,14 @@
+package org.tnsif.acc.c2tc.jr_developer;
+
+public class KIA_Airbags {
+	public void disp_airbags()
+	{
+		System.out.println("All KIA have Airbag");
+	}
+	public static void main(String[] args) {
+		KIA_Speedlimit speed=new KIA_Speedlimit();
+		speed.disp_Speedlimit();
+	}
+	
+}
+ 
