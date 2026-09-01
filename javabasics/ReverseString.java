4752f7818357c55b30e59214a96686baa4633f30
Sudharani_S <sudharanibukka2004@gmail.com>
2026-09-01 18:53:56 +0530

    Add files via upload

---
+++ javabasics/ReverseString.java
@@ -0,0 +1,17 @@
+package org.tnsif.acc.c2tc.javabasics;
+
+public class ReverseString {
+	public static void main(String[] args) {
+		
+        String str = "Java Program";
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+        System.out.println("Orginal String: " + str);
+        System.out.println("Reversed String: " + rev);
+    }
+}
+
+
