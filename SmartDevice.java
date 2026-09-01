eda7701d91e82b8b337222a9aaa4420b3785b9d9
Sudharani_S <sudharanibukka2004@gmail.com>
2026-09-01 11:10:03 +0530

    Delete SmartDevice.java

--- SmartDevice.java
+++
@@ -1,9 +0,0 @@
-package org.tnsif.acc.c2tc.interfacedemo;
-
-public interface SmartDevice {
-	
-	public abstract void turnOn();
-	public abstract void turnOff();
-	public abstract void getStatus();
-
-}
